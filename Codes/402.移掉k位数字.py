ae9648719e64d4bd4299d0ff79e7ab1a17cc713c
liuyang0001 <liuyang0001@outlook.com>
2020-05-16 19:38:43 +0800

    更新源码至405题

---
+++ Codes/402.移掉k位数字.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=402 lang=python3
+#
+# [402] 移掉K位数字
+#
+
+# @lc code=start
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        if len(num) <= k: return '0'
+        stack = []
+        
+        for n in num:
+            while stack and n < stack[-1] and k > 0:
+                stack.pop()
+                k -= 1
+            stack.append(n)
+        
+        # 现在栈里是递增数列，k还没用完，当然是从大的开始删了
+        for _ in range(k):
+            stack.pop()
+        
+        ans = ''.join(stack).lstrip('0')
+        # ans可能被strip空了
+        return ans or '0'
+# @lc code=end
+
